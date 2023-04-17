a8356188654f54d44edab7ffdc1868f12ad1e6d8
Thuniinae <thunia4182@gmail.com>
2023-04-17 12:13:06 +0800

    .

--- adder.h
+++ adder.h
@@ -24,9 +24,11 @@ private:
     
     void my_thread(void);
     void blocking_transport(tlm::tlm_generic_payload &payload, sc_core::sc_time &delay);
-    sc_fifo<int> a, b, s;
-    sc_ufixed_fast<53,10> bufferF[3];
-    sc_ufixed_fast<53,10> _a, _b, _r;
+    sc_fifo<float> a, b, s;
+    //sc_ufixed_fast<53,10> bufferF[3];
+    //sc_ufixed_fast<53,10> _a, _b, _r;
+    float bufferF[3];
+    float _a, _b, _r;
 };
 
 void ADDER::my_thread(){
@@ -36,14 +38,14 @@ void ADDER::my_thread(){
     for(int i = 0; i < 63; i++){
         _r = bufferF[2]*1/2 + bufferF[1]*1/3 + bufferF[0]*1/6;
         wait(3, SC_NS);
-        r.write(_r);
+        s.write(_r);
         bufferF[0] = bufferF[2];
         bufferF[1] = a.read();
         bufferF[2] = a.read();
     }
-    _r = buffer[2]*1/2 + buffer[1]*1/3 + buffer[0]*1/6;
+    _r = bufferF[2]*1/2 + bufferF[1]*1/3 + bufferF[0]*1/6;
     wait(3, SC_NS);
-    r.write(_r);
+    s.write(_r);
 }
 
 
@@ -59,14 +61,14 @@ void ADDER::blocking_transport(tlm::tlm_generic_payload &payload, sc_core::sc_ti
             delay = sc_time(1, SC_NS); // read delay
             switch(addr){
                 case ADDER_OUTPUT:
-                    _s = s.read();
+                    _r = s.read();
                     // buffer.uc[0] = s; // if data s has size of unsigned char
                     break;
                 default: 
                     cout << "invalid read address: " << addr << endl;
                     break;
             }
-            memcpy(data_ptr, &_s, 4);
+            memcpy(data_ptr, &_r, 4);
             break;
         case tlm::TLM_WRITE_COMMAND:
             delay = sc_time(1, SC_NS); //write delay
